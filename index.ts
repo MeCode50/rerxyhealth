735932b1f393a43873ab5903c22aa968da3b2378
Abiodun-code <olaabiodun157@gmail.com>
2023-08-22 00:18:16 -0700

    Update depencency

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-import express, { Application, Response, Request } from "express";
+import express, { Application, Response, Request, NextFunction } from "express";
 import { StatusCode } from "./enums/status";
 import { PORT } from "./constant";
 
 const app: Application = express();
+
+app.use(express.json())
+
+app.use((req:Request, res:Response, next:NextFunction)=>{
+    console.log(req.path, req.method)
+    next()
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.status(StatusCode.OK).send(`Welcome to RexHealth. Server is running on port ${PORT}`);

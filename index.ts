f54a340365707f05f1eaad948f1cca68067be50e
Abiodun-code <olaabiodun157@gmail.com>
2023-08-23 09:21:41 -0700

    Signin Auth Update

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express, { Application, Response, Request } from "express";
 import { StatusCode } from "./enums/status";
 import { PORT } from "./constant";
+import { createUser } from "./controllers/auth_controller";
 
 const app: Application = express();
+
+app.post("/auth/register", createUser)
 
 app.get("/", (req: Request, res: Response) => {
   res

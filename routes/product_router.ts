a79a7996d236f43b90d20bcb088e20c7f8804dd5
Abiodun-code <olaabiodun157@gmail.com>
2023-08-30 12:10:13 -0700

    adding middleware to product

--- routes/product_router.ts
+++ routes/product_router.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllProduct, createProduct } from "../controllers/products/product_controller";
+import { getAllProduct, createProduct, deleteProduct } from "../controllers/products/product_controller";
+import { isAuthenticated } from "../middleware/isAuthenticated";
 
 const router = express.Router()
 
-router.get("/product", getAllProduct)
-router.post("/create", createProduct)
+router.get("/product", isAuthenticated, getAllProduct)
+router.post("/create", isAuthenticated, createProduct)
+router.delete("/prodct/id", isAuthenticated, deleteProduct)
 
 const productRouter = router
 export default productRouter

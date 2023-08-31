81b1c7f7017cee2342c250f6195c89a3a3ed9379
yhoungdev <yhoungdev@gmail.com>
2023-08-31 10:12:05 +0100

    chore: update

--- routes/product_router.ts
+++ routes/product_router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { getAllProduct, createProduct } from "../controllers/products/product_controller";
+import { isAuthenticated } from "../middleware/isAuthenticated";
 
 const router = express.Router()
 
-router.get("/product", getAllProduct)
-router.post("/create", createProduct)
+router.get("/product", isAuthenticated ,getAllProduct)
+router.post("/create", isAuthenticated ,createProduct)
 
 const productRouter = router
 export default productRouter

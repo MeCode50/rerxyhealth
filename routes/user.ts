39f3341273d32ae71cd4b95a531dab8926c1c30c
yhoungdev <yhoungdev@gmail.com>
2023-10-10 04:28:03 +0100

    feat: included endpoint to retrive Transaction Pin

--- routes/user.ts
+++ routes/user.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/isAuthenticated";
 import { getUserProfile, updateUserProfile } from "../controllers/profile";
+import { get_transaction_pin } from "../controllers/users/get_transaction_pin";
 
 const router = express.Router();
 
 router.get("/profile/me", isAuthenticated, getUserProfile);
 router.put("/profile/update", isAuthenticated, updateUserProfile);
+router.get("/profile/transaction_pin" , isAuthenticated, get_transaction_pin)
 
 const profileRouter = router;
 export default profileRouter;

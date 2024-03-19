7bffe77e3b4ef6ef3ccd1fecf5930475ee8b9bc7
Munachimso <Victorycollins31@gmail.com>
2024-03-19 15:59:36 +0100

    🎨 Add: added content type to verify tranaction

--- helper/verify_transaction.ts
+++ helper/verify_transaction.ts
@@ -4,17 +4,18 @@ require("dotenv").config();
 
 //function to verify transaction with paystack 
 export async function verifyTransaction(reference:string) {
-    try {
-        const response = await axios.get('${process.env.PAYSTACK_VERIFY_URL}/${reference}', {
-            headers: {
-                'Authorization': 'Bearer ${process.env.PAYSTACK_SECRET}'
-            }
-        });
-        return response.data;
-    } catch (error:any) {
-        console.error('Error verifying transaction', error.response.data);
-        throw error;
-    }
+try {
+    const response = await axios.get('${process.env.PAYSTACK_VERIFY_URL}/${reference}', {
+        headers: {
+            'Authorization': 'Bearer ${process.env.PAYSTACK_SECRET}',
+            "Content-Type": "application/json",
+        }
+    });
+    return response.data;
+} catch (error:any) {
+    console.error('Error verifying transaction', error.response.data);
+    throw error;
+}
 }
 
 

b6b1784677df8cedbd163a836606e336c7ba6acc
Munachimso <Victorycollins31@gmail.com>
2024-03-21 10:18:07 +0100

    ✅ Update: added function to create a diagnostic test by admin

--- controllers/admin/diagnostics/admin_controller.ts
+++ controllers/admin/diagnostics/admin_controller.ts
@@ -5,8 +5,23 @@ import { StatusCode } from "../../../enums/status";
 // create test 
 const createDiagnosticTest = async (req: Request, res: Response) => {
     try {
-  //      const {name, price, category}
+        const { name, price, categoryId } = req.body;
+
+        //validate data entered 
+        if (!name || !price || !categoryId) {
+            return res.status(StatusCode.BadRequest).json({ message: 'Missing required field' });
+        }
+
+        // create test 
+        const diagnosticTest = await prisma.diagnosticTest.create({
+            data: {
+                name,
+                price,
+                categoryId,
+            },
+        });
+        res.status(StatusCode.Created).json({ diagnosticTest });
     } catch (error) {
-        
+        res.status(StatusCode.InternalServerError).json({ message: "Error creating diagnostic test", error })
     }
-}
+};

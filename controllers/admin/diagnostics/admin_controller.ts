c0402fef5634761f6fa8873f9c112233437c6fce
Munachimso <Victorycollins31@gmail.com>
2024-03-21 01:40:42 +0100

    ✅ Update: update the database model with diagnostics

---
+++ controllers/admin/diagnostics/admin_controller.ts
@@ -0,0 +1,12 @@
+import prisma from "../../../prisma";
+import { Request, Response } from "express";
+import { StatusCode } from "../../../enums/status";
+
+// create test 
+const createDiagnosticTest = async (req: Request, res: Response) => {
+    try {
+  //      const {name, price, category}
+    } catch (error) {
+        
+    }
+}

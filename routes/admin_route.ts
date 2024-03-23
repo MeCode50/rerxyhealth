3a880d8503ed0bef24e3d388e8a078fc670d23c8
Munachimso <Victorycollins31@gmail.com>
2024-03-23 20:29:49 +0100

    🔥 UPDATE; updated the admin route

--- routes/admin_route.ts
+++ routes/admin_route.ts
@@ -6,6 +6,11 @@ import { admin_get_all_appointments } from "../controllers/admin/appointments/ad
 import { adminCreateProduct } from "../controllers/admin/product/create_product";
 import { adminGetProduct } from "../controllers/admin/product/get_all_product";
 import { adminDeleteProduct } from "../controllers/admin/product/delete_product";
+import {createDiagnosticTest,
+  getAllDiagnosticTest,
+  getTestById,
+  updatedTestById,
+  deleteTestBtId,} from "../controllers/admin/diagnostics/admin_controller"
 
 const app = express.Router();
 
@@ -23,7 +28,11 @@ app.get("/admin/product/all", adminGetProduct)
 app.delete("/admin/product/delete/:id", adminDeleteProduct)
 
 // diagnostic test section 
-
+app.post("/admin/diagnostic/create", createDiagnosticTest);
+app.get("/admin/diagnostic/all", getAllDiagnosticTest);
+app.get("/admin/diagnostic/:id", getTestById);
+app.put("/admin/diagnostic/edit/:id", updatedTestById);
+app.delete("/admin/diagnostic/delete/:id", deleteTestBtId);
 
 const adminRouter = app;
 export default adminRouter;
